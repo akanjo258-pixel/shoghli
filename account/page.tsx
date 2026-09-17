88f796f8df56485327872b230843a543033cbaba
akanjo258-pixel <akanjo258@gmail.com>
2026-09-17 10:42:00 +0300

    Create Account component with form inputs

---
+++ account/page.tsx
@@ -0,0 +1,23 @@
+export default function Account() {
+  return (
+    <main dir="rtl">
+      <h1>👤 حسابي</h1>
+
+      <input placeholder="الاسم الكامل" />
+      <br /><br />
+
+      <input placeholder="البريد الإلكتروني" />
+      <br /><br />
+
+      <input placeholder="المدينة" />
+
+      <br /><br />
+
+      <button>حفظ البيانات</button>
+
+      <br /><br />
+
+      <a href="/">← الرئيسية</a>
+    </main>
+  );
+}
