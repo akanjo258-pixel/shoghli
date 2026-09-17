9879ad52860f436ba768cd192cfd7cf5b2d021bb
akanjo258-pixel <akanjo258@gmail.com>
2026-09-17 10:42:27 +0300

    Create PostJob component for job posting form

---
+++ post-job/page.tsx
@@ -0,0 +1,26 @@
+export default function PostJob() {
+  return (
+    <main dir="rtl">
+      <h1>📢 نشر وظيفة</h1>
+
+      <input placeholder="اسم الوظيفة" />
+      <br /><br />
+
+      <input placeholder="اسم الشركة" />
+      <br /><br />
+
+      <input placeholder="المدينة" />
+      <br /><br />
+
+      <textarea placeholder="وصف الوظيفة" />
+
+      <br /><br />
+
+      <button>نشر الوظيفة</button>
+
+      <br /><br />
+
+      <a href="/">← الرئيسية</a>
+    </main>
+  );
+}
